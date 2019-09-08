4b30056c7f01cea1be867ab12549da8e17e55bd8
Cindy Liu <cindyliu923@gmail.com>
2019-09-08 21:46:49 +0800

    Update chick.rb

--- chick.rb
+++ chick.rb
@@ -13,42 +13,45 @@ def line
 end
 
 # 取得對方說的話
-def received_text
+def received_message
   @params = JSON.parse request.body.read
-  message = @params['events'][0]['message']
-  message['text'] unless message.nil?
+  @params['events'][0]['message']
 end
 
 # 關鍵字回覆
-def keyword_reply(received_text)
-  received_text.tr('嗎', '').tr('?？', '!！')
+def keyword_reply(message)
+  message['text'].tr('嗎', '').tr('?？', '!！')
+end
+
+# 設定回覆訊息
+def reply_message
+  message = received_message
+  case message['type']
+  when 'text'
+    { type: 'text',
+      text: keyword_reply(message) }
+  when 'sticker'
+    { type: 'sticker',
+      packageId: '11537',
+      stickerId: '52002753' }
+  end
 end
 
 # 傳送訊息到 line
-def reply_to_line(reply_text)
-  return nil if reply_text.nil?
+def reply_to_line(reply_message)
+  return nil if reply_message.nil?
 
   # 取得 reply token
   reply_token = @params['events'][0]['replyToken']
 
-  # 設定回覆訊息
-  message = {
-    type: 'text',
-    text: reply_text
-  }
-
   # 傳送訊息
-  line.reply_message(reply_token, message)
+  line.reply_message(reply_token, reply_message)
 end
 
 post '/chick/webhook' do
-
-  # 設定回覆文字
-  reply_text = keyword_reply(received_text)
   # 傳送訊息到 line
-  response = reply_to_line(reply_text)
+  response = reply_to_line(reply_message)
 
   # 回應 200
   :ok
-
 end
